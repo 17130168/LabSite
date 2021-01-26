fef3aea597590aab42f54bd69db1615135d9ffa5
RomanKV <roman.krilow@mail.ru>
2021-01-26 21:10:00 +0500

    Drop-down list

--- edit_keys.php
+++ edit_keys.php
@@ -1,8 +1,9 @@
 <html>
-<head
+<head> 
 <title> Редактирование данных о ключе </title>
 </head>
-<body> 
+<body>
+<form action='save_edit_keys.php' method='get'>
 <?php
 $link = new mysqli("localhost", "f0474370_RKVGames", "1123", "f0474370_RKVGames");
 if ($link->connect_errno) {
@@ -10,22 +11,39 @@ echo "Не удалось подключиться к серверу";
 }
 $id_key =  $_GET['id_key'];
 $result = $link->query("SELECT key_buy, key_end, id_game, id_store, key_sell, key_type FROM `Keys` WHERE id_key = '$id_key'");
+print "Код ключа: <input name='new_id_key' size='50' type='text' value='$id_key'>";
 if ($result) {
 while ($st = $result->fetch_array()) {
 $key_buy = $st['key_buy'];
 $key_end = $st['key_end'];
-$id_game = $st['id_game'];
-$id_store = $st['id_store'];
 $key_sell = $st['key_sell'];
 $key_type = $st['key_type'];
 }
 }
-print "<form action='save_edit_keys.php' method='get'>";
-print "Код ключа: <input name='new_id_key' size='50' type='text' value='$id_key'>";
-print "<br>Дата покупки ключа: <input name='key_buy' size='20' type='date' value='$key_buy'>";
+print "<br>Дата покупки ключа: <input name='key_buy' size='20' type='date' placeholder='dd-mm-yyyy' value='$key_buy'>";
 print "<br>Дата аннулирования ключа: <input name='key_end' size='20' type='date' placeholder='dd-mm-yyyy' value='$key_end'>";
-print "<br>Код игры: <input name='id_game' size='20' type='text' placeholder='dd-mm-yyyy' value='$id_game'>";
-print "<br>Код магазина: <input name='id_store' size='20' type='text' value='$id_store'>";
+$result = $link->query("SELECT id_game, name FROM `Games`");
+echo "<br>Игра: <select name='id_game'>";
+echo "<option selected value='$id_game'>$name</option>";
+if ($result) {
+while ($row = $result->fetch_array()) {
+$id_game = $row['id_game'];
+$name = $row['name'];
+echo "<option value='$id_game'>$name</option>";
+}
+}
+echo "</select>";
+$result = $link->query("SELECT id_store, name_store FROM `Stores`");
+echo "<br>Магазин: <select name='id_store'>";
+echo "<option selected value='$id_store'>$name_store</option>";
+if ($result) {
+while ($row = $result->fetch_array()) {
+$id_store = $row['id_store'];
+$name_store = $row['name_store'];
+echo "<option value='$id_store'>$name_store</option>";
+}
+}
+echo "</select>";
 print "<br>Цена ключа: <input name='key_sell' size='20' type='text' value='$key_sell'>";
 print "<br>Тип ключа: <input name='key_type' size='20' type='text' value='$key_type'>";
 print "<input type='hidden' name='id_key' size='30' value='$id_key'>";
--- new_keys.php
+++ new_keys.php
@@ -6,8 +6,32 @@
 Код ключа: <input name="id_key" size="20" type="text">
 <br>Дата покупки ключа: <input name="key_buy" size="20" type="date" placeholder="dd-mm-yyyy">
 <br>Дата аннулирования ключа: <input name="key_end" size="20" type="date" placeholder="dd-mm-yyyy">
-<br>Код игры: <input name="id_game" size="20" type="text">
-<br>Код магазина: <input name="id_store" size="20" type="text">
+<?php
+$link = new mysqli("localhost", "f0474370_RKVGames", "1123", "f0474370_RKVGames");
+if ($link->connect_errno) {
+echo "Не удалось подключиться к серверу";
+}
+$result = $link->query("SELECT id_game, name FROM `Games`");
+echo "<br>Игра: <select name='id_game'>";
+if ($result) {
+while ($row = $result->fetch_array()) {
+$id_game = $row['id_game'];
+$name = $row['name'];
+echo "<option value='$id_game'>$name</option>";
+}
+}
+echo "</select>";
+$result = $link->query("SELECT id_store, name_store FROM `Stores`");
+echo "<br>Магазин: <select name='id_store'>";
+if ($result) {
+while ($row = $result->fetch_array()) {
+$id_store = $row['id_store'];
+$name_store = $row['name_store'];
+echo "<option value='$id_store'>$name_store</option>";
+}
+}
+echo "</select>";
+?>
 <br>Цена ключа: <input name="key_sell" size="20" type="text">
 <br>Тип ключа: <input name="key_type" size="20" type="text">
 <p><input name="add" type="submit" value="Добавить"> <input name="b2" type="reset" value="Очистить"></p>
